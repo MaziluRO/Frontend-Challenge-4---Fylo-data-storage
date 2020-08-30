45531601098f2a5de2f4dfd89ddbc86c078a2b3d
MaziluRO <41089729+MaziluRO@users.noreply.github.com>
2020-08-30 14:28:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 var slider = document.getElementById("mySlider");
 var output = document.getElementById("used-GB");
 var remaining = document.getElementById("bubble-value");
-var selector = document.getElementById("selector-button");
 var progress = document.getElementById("progress-bar");
 
 output.innerHTML = slider.value;
 remaining.innerHTML = 1000 - slider.value;
+
 slider.oninput = function () {
-  selector.style.left = this.value / 10 + "%";
+  progress.style.width = this.value / 10 + "%";
   output.innerHTML = this.value;
   remaining.innerHTML = 1000 - this.value;
-  progress.style.width = this.value / 10 + "%";
 };
